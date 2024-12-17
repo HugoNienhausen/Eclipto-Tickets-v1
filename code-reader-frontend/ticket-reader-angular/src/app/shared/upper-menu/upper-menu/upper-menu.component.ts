d27479ae8c777447f6962374d3f1ac97dc77f438
HugoNienhausen <hugo.nienhausen@gmail.com>
2024-12-18 00:46:34 +0100

    Sidebar yupper menu responsive, se ha añadido boton de cerrar sesión y perfil

--- code-reader-frontend/ticket-reader-angular/src/app/shared/upper-menu/upper-menu/upper-menu.component.ts
+++ code-reader-frontend/ticket-reader-angular/src/app/shared/upper-menu/upper-menu/upper-menu.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-upper-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './upper-menu.component.html',
   styleUrl: './upper-menu.component.css'
 })
 export class UpperMenuComponent {
+  @Input() isSidebarCollapsed: boolean = false;
+  isProfileMenuOpen: boolean = false;
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  toggleProfileMenu() {
+    this.isProfileMenuOpen = !this.isProfileMenuOpen;
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/welcome']);
+  }
+
+  goToProfile() {
+    this.router.navigate(['/user/profile']);
+    this.isProfileMenuOpen = false;
+  }
 }
