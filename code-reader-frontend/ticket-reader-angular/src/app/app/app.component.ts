771bbaa2a74a85d6edab5559afa053ecfeabd373
HugoNienhausen <hugo.nienhausen@gmail.com>
2024-12-21 23:45:05 +0100

    nav button quitado, se ha mejorado la interfaz

--- code-reader-frontend/ticket-reader-angular/src/app/app/app.component.ts
+++ code-reader-frontend/ticket-reader-angular/src/app/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavButtonComponent } from '../shared/components/nav-button/nav-button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavButtonComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
